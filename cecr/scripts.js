9df230f9874e349634a0cc0b1536a1d447efde49
Pierre Laporte <pierre@pingtimeout.fr>
2024-05-11 23:00:16 +0200

    Add Agility

--- cecr/scripts.js
+++ cecr/scripts.js
@@ -1,10 +1,11 @@
 const BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const trainers = [
-    'Benoit', 'Clem', 'Fred N', 'Jean Yves', 'Laura', 'Marine',
-    'Muriel', 'Olivier L', 'Olivier P', 'Pierre', 'Sylvie R', 'Virginie',
+    'Benoit', 'Clem', 'Fred N', 'Jean Yves', 'Laura', 'Marine', 'Muriel',
+    'Olivier L', 'Olivier P', 'Pierre', 'Sylvie B', 'Sylvie R', 'Virginie',
 ];
 const activities = [
     'Canicross',
+    'Agility',
     'Dog Dancing',
     'Hooper',
     'Accueil',
